0ed0a55155f085bf6a02464c3648c5df86a92263
MicaelAzarias <143201335+MicaelAzarias@users.noreply.github.com>
2023-11-30 19:50:15 -0300

    contato.js

--- contato.js
+++ contato.js
@@ -1,4 +1,3 @@
-
 var btnContatos = document.querySelector("#contatos");
 var btnConta = document.querySelector("#conta");
 
@@ -12,3 +11,41 @@ btnContatos.addEventListener("click", function () {
 btnConta.addEventListener("click", function() {
     body.className = "conta-js";
 });
+
+//------------------------------------------------- 
+
+const form = document.getElementById("formulario");
+const nameInput = document.getElementById("username");
+const emailInput = document.getElementById("email");
+
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    //validação se o nome esta vazio 
+    if(nameInput.value === "") {
+        alert("Por favor, preencha o seu nome");
+        return;
+    }
+
+    if (emailInput.value === "" || !isEmailValid(emailInput.value)) {
+        alert("por favor, preencha seu email");
+        return;
+    }
+
+    form.submit()
+})
+
+
+function isEmailValid(email) {
+
+    const emialRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if(emialRegex.test(email)) {
+        return true;
+    }
+
+    return false;
+}
